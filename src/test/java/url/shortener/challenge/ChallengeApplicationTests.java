ff8111ac9e50c95aae8b93c8ba68dc174199d35f
abderrahimraqi97 <abderrahim.raqi97@gmail.com>
2025-09-28 20:25:42 +0100

    Project setup initialisation

---
+++ src/test/java/url/shortener/challenge/ChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package url.shortener.challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
